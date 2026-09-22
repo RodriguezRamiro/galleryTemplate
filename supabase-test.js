ef1a0b539d4135943d00f181203b242d22c9d0fd
Ramiro Rodriguez <rodriguezcodesolutions@gmail.com>
2026-09-22 12:42:11 -0400

    replaced file with console log to test functionality

--- supabase-test.js
+++ supabase-test.js
@@ -1,8 +1,2 @@
-const SUPABASE_URL =
-    "https://sgsenlnkfpfvgewhqgxn.supabase.co";
-
-const SUPABASE_ANON_KEY =
-    "YOUR_PUBLISHABLE_OR_ANON_KEY_HERE";
-
-console.log("Supabase URL:", SUPABASE_URL);
-console.log("Supabase client test ready.");
+console.log("Supabase library:", window.supabase);
+console.log("Supabase test file loaded.");
